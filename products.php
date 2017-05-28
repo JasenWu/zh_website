db67632eeeb7e760f2498a4e07d4d13c14ba1f30
jason <447124329@qq.com>
2017-05-28 10:30:52 +0800

    add pages

--- products.php
+++ products.php
@@ -51,21 +51,58 @@ include 'common/header.php';
                     <?php
 
                     $catid = $_GET["catid"];
-                    $sql_list = "SELECT * FROM `v9_category` WHERE `parentId` =".$catid;
+                    $catid__new = "";
+                    if($catid == '42'){
+                        $catid__new = 43;
+                    }
+
+
+                    //分页功能
+                    $rowcount= mysqli_fetch_array(mysqli_query($con,"SELECT COUNT(*) AS C FROM `v9_news` WHERE `catid` =".$catid__new))['C'];//数据总数量
+
+
+
+
+                    if(is_array($_GET)&&count($_GET)>0)//判断是否有Get参数
+                    {
+                        if(isset($_GET["page"]))//判断所需要的参数是否存在，isset用来检测变量是否设置，返回true or false
+                        {
+                            $cur_page = $_GET["page"];//存在
+                        }else{
+                            $cur_page= 1;//存在
+                        }
+                    }
+
+                    $numbersPerPage = 2;//每页产品数
+
+                    $totalPageNum = $rowcount / $numbersPerPage;//页总数
+                    $startIndex = ($cur_page  -1) * $numbersPerPage;//页总数
+
+                    printf("总共返回行数据:".$rowcount."<br/>");
+
+                    printf("当前页:".$cur_page."<br/>");
+                    printf("每页产品数:".$numbersPerPage."<br/>");
+                    printf("页总数:".$totalPageNum."<br/>");
+                    printf("起始index:".$startIndex."<br/>");
+
+                    //分页结束
+
+                    $sql_list = "SELECT * FROM `v9_news` WHERE `catid` = ".$catid__new." LIMIT ".$startIndex.",".$numbersPerPage;
 
                     $result_list = mysqli_query($con, $sql_list);
+
                     while ($row = mysqli_fetch_array($result_list)) {
 
 
 
                         echo " <li>
                                 <dl>
-                                    <dt><a href=\"/product2_cid_99_id_650.html\">".$row['catname']." </a><br/>
-                                        <a href=\"/product2_cid_99_id_650.html\"></a></dt>
-                                    <dd class=\"tu\"><a href=\"/product2_cid_99_id_650.html\"><img src=".$row['image']."
+                                    <dt><a href=\"/product2_cid_99_id_650.html\">".$row['title']." </a><br/>
+                                        <a href=\"/product2_cid_99_id_650.html\"></a>".$row['keywords']."</dt>
+                                    <dd class=\"tu\"><a href=\"/product2_cid_99_id_650.html\"><img src=".$row['thumb']."
                                                                                                width=\"169\" height=\"121\"/></a>
                                     </dd>
-                                    <dd class=\"zi\">适合圆柱型工件的固定</dd>
+                                    <dd class=\"zi\">".$row['description']."</dd>
                                     <dd class=\"zzi\"><a href=\"/shoufeng/台架.pdf#page=8\" target=\"_blank\">详细参数（PDF)</a></dd>
         
                                 </dl>
@@ -85,21 +122,27 @@ include 'common/header.php';
 
                 </ul>
 
+                <?php
+
+
+                ?>
+
                 <div class="page">
                     <div class="pageli">
 
 
-                        <a href="#">《</a>
+                        <a href="products.php?catid=42&page=">《</a>
 
-                        <a href="/product_page_1.html?cid=60">1</a>
+                        <?php
 
-                        <a href="/product_page_2.html?cid=60">2</a>
+                            for ($i=1; $i<=$totalPageNum; $i++)
+                            {
+                                echo "<a href='products.php?catid=42&page=".$i."' >".$i."</a>";
+                            }
 
-                        <a href="/product_page_3.html?cid=60">3</a>
+                        ?>
 
-                        <a href="/product_page_4.html?cid=60">4</a>
-
-                        <a href="/product_page_2.html?cid=60">》</a>
+                        <a href="products.php?catid=42&page=">》</a>
 
                     </div>
                 </div>
